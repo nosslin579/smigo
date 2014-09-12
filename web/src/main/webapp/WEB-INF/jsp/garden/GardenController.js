f6c70b333f9c5807372521bde54f20111183e8df
Christian <christian1195@gmail.com>
2014-09-12 14:19:38 +0200

    Moved functions from controller to service

--- web/src/main/webapp/WEB-INF/jsp/garden/GardenController.js
+++ web/src/main/webapp/WEB-INF/jsp/garden/GardenController.js
@@ -15,56 +15,11 @@ function GardenController($scope, PlantService, GardenService) {
 
     $scope.addYear = PlantService.addYear;
     $scope.model = GardenService.model;
-
-    //Action
-    //todo
-
-    $scope.onSquareClick = function (clickEvent, square) {
-        console.log('Square clicked', [clickEvent, square, $scope.model.selectedSpecies]);
-        if (clickEvent.shiftKey) {
-            PlantService.removePlant(square);
-        } else if (clickEvent.ctrlKey) {
-            console.log('Copy species');
-        } else {
-            PlantService.addPlant($scope.model.selectedSpecies, square);
-        }
-        clickEvent.stopPropagation();
-    };
-
-    $scope.onVisibleRemainderClick = function (clickEvent, s) {
-        PlantService.addSquare($scope.model.selectedYear, s.location.x, s.location.y, $scope.model.selectedSpecies);
-        PlantService.save();
-        clickEvent.stopPropagation();
-    };
-
-    $scope.onGridClick = function (clickEvent) {
-        //http://stackoverflow.com/a/14872192/859514
-        var offsetX = clickEvent.clientX - $(clickEvent.target).offset().left;
-        var offsetY = clickEvent.clientY - $(clickEvent.target).offset().top;
-        var x = Math.floor((offsetX - 100000) / 48);
-        var y = Math.floor((offsetY - 100000) / 48);
-        PlantService.addSquare($scope.model.selectedYear, x, y, $scope.model.selectedSpecies);
-        clickEvent.stopPropagation();
-    };
-
-    $scope.getGridSizeCss = function (year) {
-        var bounds = PlantService.getBounds(year);
-//        console.log('Grid size', bounds);
-        var margin = 48 * 2;
-        return {
-            'margin-top': (-100000 + -bounds.ymin * 48 + margin) + 'px',
-            'width': (100000 + 47 + bounds.xmax * 48 + margin) + 'px',
-            'height': (100000 + 47 + bounds.ymax * 48 + margin) + 'px',
-            'margin-left': (-100000 + -bounds.xmin * 48 + margin) + 'px'
-        };
-    };
-
-    $scope.getSquarePositionCss = function (square) {
-        return {
-            top: square.location.y * 48 + 100000 + 'px',
-            left: square.location.x * 48 + 100000 + 'px'
-        };
-    };
+    $scope.onSquareClick = GardenService.onSquareClick;
+    $scope.onVisibleRemainderClick = GardenService.onVisibleRemainderClick;
+    $scope.onGridClick = GardenService.onGridClick;
+    $scope.getGridSizeCss = GardenService.getGridSizeCss;
+    $scope.getSquarePositionCss = GardenService.getSquarePositionCss;
 }
 
 angular.module('smigoModule').controller('GardenController', GardenController);
--- web/src/main/webapp/WEB-INF/jsp/garden/GardenService.js
+++ web/src/main/webapp/WEB-INF/jsp/garden/GardenService.js
@@ -4,6 +4,50 @@ function GardenService(PlantService) {
         selectedYear: +Object.keys(PlantService.getGarden().squares).sort().slice(-1).pop(),
         availableYears: PlantService.getAvailableYears()
     };
-    return {model: model}
+    return {
+        model: model,
+        onSquareClick: function (clickEvent, square) {
+            console.log('Square clicked', [clickEvent, square, model.selectedSpecies]);
+            if (clickEvent.shiftKey) {
+                PlantService.removePlant(square);
+            } else if (clickEvent.ctrlKey) {
+                console.log('Copy species');
+            } else {
+                PlantService.addPlant(model.selectedSpecies, square);
+            }
+            clickEvent.stopPropagation();
+        },
+        onVisibleRemainderClick: function (clickEvent, square) {
+            PlantService.addSquare(model.selectedYear, square.location.x, square.location.y, model.selectedSpecies);
+            PlantService.save();
+            clickEvent.stopPropagation();
+        },
+        onGridClick: function (clickEvent) {
+            //http://stackoverflow.com/a/14872192/859514
+            var offsetX = clickEvent.clientX - $(clickEvent.target).offset().left;
+            var offsetY = clickEvent.clientY - $(clickEvent.target).offset().top;
+            var x = Math.floor((offsetX - 100000) / 48);
+            var y = Math.floor((offsetY - 100000) / 48);
+            PlantService.addSquare(model.selectedYear, x, y, model.selectedSpecies);
+            clickEvent.stopPropagation();
+        },
+        getGridSizeCss: function (year) {
+            var bounds = PlantService.getBounds(year);
+//        console.log('Grid size', bounds);
+            var margin = 48 * 2;
+            return {
+                'margin-top': (-100000 + -bounds.ymin * 48 + margin) + 'px',
+                'width': (100000 + 47 + bounds.xmax * 48 + margin) + 'px',
+                'height': (100000 + 47 + bounds.ymax * 48 + margin) + 'px',
+                'margin-left': (-100000 + -bounds.xmin * 48 + margin) + 'px'
+            };
+        },
+        getSquarePositionCss: function (square) {
+            return {
+                top: square.location.y * 48 + 100000 + 'px',
+                left: square.location.x * 48 + 100000 + 'px'
+            };
+        }
+    }
 }
 angular.module('smigoModule').factory('GardenService', GardenService);
