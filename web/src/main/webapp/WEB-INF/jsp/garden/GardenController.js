855c77965a66ea578700945b2d2fd625cc877092
Christian <christian1195@gmail.com>
2014-09-12 13:56:52 +0200

    Refactor squares

--- web/src/main/webapp/WEB-INF/jsp/garden/GardenController.js
+++ web/src/main/webapp/WEB-INF/jsp/garden/GardenController.js
@@ -1,15 +1,19 @@
 function GardenController($scope, PlantService, GardenService) {
     console.log('GardenController', $scope);
 
-    //Garden
-    $scope.$watch(PlantService.getGarden, function () {
-        $scope.garden = PlantService.getGarden();
-        $scope.forwardYear = +Object.keys(PlantService.getGarden().squares).sort().slice(-1).pop() + 1;
-        $scope.backwardYear = +Object.keys(PlantService.getGarden().squares).sort()[0] - 1;
-    }, true);
+    $scope.$watch('model.selectedYear', function (newYear) {
+        $scope.squares = PlantService.getGarden().squares[newYear];
+        $scope.visibleRemainderSquares = PlantService.getGarden().squares[newYear - 1];
+    });
+
+    $scope.species = PlantService.getGarden().species;
+
+    $scope.$watch('model.availableYears', function (newYears) {
+        $scope.forwardYear = newYears.slice(-1).pop() + 1;
+        $scope.backwardYear = newYears[0] - 1;
+    });
 
     $scope.addYear = PlantService.addYear;
-
     $scope.model = GardenService.model;
 
     //Action
