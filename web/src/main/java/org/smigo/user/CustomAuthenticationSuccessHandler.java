1c1162ef5cece6a7918a1ad7b16fb51ce187aa1f
Christian <christian1195@gmail.com>
2014-09-22 10:39:59 +0200

    pom fix

--- web/src/main/java/org/smigo/user/CustomAuthenticationSuccessHandler.java
+++ web/src/main/java/org/smigo/user/CustomAuthenticationSuccessHandler.java
@@ -1,7 +1,6 @@
 package org.smigo.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +40,7 @@ class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler
         } else {
             String userAsJson = objectMapper.writeValueAsString(user);
             response.getWriter().append(userAsJson);
-            response.setHeader(HttpHeaders.CONTENT_TYPE, "application/json;charset=UTF-8");
+            response.setHeader("Content-Type", "application/json;charset=UTF-8");
             response.setStatus(HttpStatus.OK.value());
         }
     }
