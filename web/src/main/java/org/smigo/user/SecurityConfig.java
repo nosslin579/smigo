85af3e60e05d189a5523aa56abece2a7df190d99
Christian <christian1195@gmail.com>
2014-09-23 19:55:54 +0200

    Comment about disableUrlRewrite

--- web/src/main/java/org/smigo/user/SecurityConfig.java
+++ web/src/main/java/org/smigo/user/SecurityConfig.java
@@ -48,6 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+/*
+        HttpSessionSecurityContextRepository repository = new HttpSessionSecurityContextRepository();
+        repository.setDisableUrlRewriting(false);
+        http.securityContext().securityContextRepository(repository);
+*/
         http.authorizeRequests()
                 //Garden
                 .antMatchers("/savegarden/**").permitAll()
