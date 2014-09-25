f282d6c65bb5c44660ae6c0dad4ef3a22f0bf502
Christian <christian1195@gmail.com>
2014-09-25 16:22:16 +0200

    Collapse fix

--- web/src/main/webapp/WEB-INF/jsp/MainMenuController.js
+++ web/src/main/webapp/WEB-INF/jsp/MainMenuController.js
@@ -1,6 +1,7 @@
 function MainMenuController(UserService, $scope) {
     $scope.logout = UserService.logout;
     $scope.userState = UserService.getState();
+    $scope.menuIsCollapsed = true;
 }
 
 angular.module('smigoModule').controller('MainMenuController', MainMenuController);
