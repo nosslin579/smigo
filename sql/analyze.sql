d9cbf6408b1f6f5bb52afed9a8b6a6e0eca32a90
Christian <christian1195@gmail.com>
2014-10-03 19:31:29 +0200

    Version 4 database scripts update

--- sql/analyze.sql
+++ sql/analyze.sql
@@ -2,7 +2,7 @@ SHOW PROCESSLIST;
 
 SELECT
   users.username,
-  user_id,
+  id,
   regtime,
   decidetime,
   users.createdate,
@@ -12,31 +12,12 @@ SELECT
   count(IF(year = 2014, 1, NULL)) AS y2014,
   count(IF(year = 2015, 1, NULL)) AS y2015
 FROM users
-  LEFT JOIN plants ON plants.fkuserid = users.user_id
+  LEFT JOIN plants ON plants.fkuserid = users.id
   LEFT JOIN (SELECT
                username,
                count(*)                  AS requests,
                count(DISTINCT sessionid) AS sessions
              FROM visitlog
              GROUP BY username) AS r ON r.username = users.username
-GROUP BY user_id
-ORDER BY user_id DESC;
-
-
-SELECT
-  *
-FROM (SELECT
-        user_id
-      FROM users
-      WHERE user_id NOT IN (
-        SELECT
-          fkuserid
-        FROM plants
-          JOIN species ON plants.species = species.species_id
-        WHERE item = TRUE OR annual = FALSE
-        GROUP BY fkuserid)) sinners JOIN (SELECT
-                                            fkuserid,
-                                            count(*) AS no
-                                          FROM plants
-                                          GROUP BY fkuserid) noofplants ON sinners.user_id = noofplants.fkuserid;
-
+GROUP BY id
+ORDER BY id DESC;
