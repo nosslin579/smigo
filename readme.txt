422f7b51ae682e65db80ea242dfb5d21e999c264
Christian <christian1195@gmail.com>
2014-10-07 15:23:40 +0200

    Update logging

--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 System properties:
--Dhost.env.id=dev
 -Dlogback.configurationFile=/home/nosslin2/_java/smigo/web/logback-dev.xml
+-Dfile.encoding=UTF-8
 
 Copy context.xml to ./tomcat/conf
--- web/src/test/java/org/smigo/SandboxTest.java
+++ web/src/test/java/org/smigo/SandboxTest.java
@@ -2,7 +2,6 @@ package org.smigo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,10 +10,12 @@ public class SandboxTest {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private BCryptPasswordEncoder a = new BCryptPasswordEncoder();
-
     public void testAdd() throws Exception {
-        a.matches("testreg17", "$2a$10$r1TvEVQWyOYJJ3yvc8qLbOyA3LdsrM63bPFjoOB/ITEXGllcV4iI.");
+        log.trace("trace");
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
         Assert.assertTrue(true);
 
     }
