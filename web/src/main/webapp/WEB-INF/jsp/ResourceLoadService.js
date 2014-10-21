11e2faae9527461fc222cad59affcb84722624bd
Christian <christian1195@gmail.com>
2014-10-21 11:40:22 +0200

    ResourceLoader added

---
+++ web/src/main/webapp/WEB-INF/jsp/ResourceLoadService.js
@@ -0,0 +1,35 @@
+function ResourceLoadService($http, $window, $timeout, $rootScope, $q, $log) {
+
+    $window.fbAsyncInit = function () {
+        $log.debug('Init facebook plugin');
+        FB.init({
+            appId: '1486322734976014',
+            status: false,
+            cookie: true,
+            version: 'v2.1',
+            xfbml: false
+        });
+    };
+
+    function load(url, id) {
+        var s = 'script';
+        $log.debug('Retrieving javascript ', [url, id, document.getElementsByTagName(s)]);
+        var js, fjs = document.getElementsByTagName(s)[0];
+        if (document.getElementById(id)) return;
+        js = document.createElement(s);
+        js.id = id;
+        js.src = url;
+        fjs.parentNode.insertBefore(js, fjs);
+    };
+
+    return {
+        facebook: function () {
+            load('//connect.facebook.net/en/sdk.js', 'facebook-jssdk');
+        },
+        twitter: function () {
+            load('//platform.twitter.com/widgets.js', 'twitter-widgets');
+        }
+    }
+
+}
+angular.module('smigoModule').factory('ResourceLoadService', ResourceLoadService);
