bf4e156da24e85a7d0d7cf575bb18c1d52688128
Christian <christian1195@gmail.com>
2014-10-22 19:58:07 +0200

    Nbsp fix

--- web/src/main/webapp/WEB-INF/jsp/social/TwitterDirective.js
+++ web/src/main/webapp/WEB-INF/jsp/social/TwitterDirective.js
@@ -5,7 +5,7 @@ function TwitterDirective($log, $timeout) {
             ngModel: '='
         },
         require: '^ngModel',
-        template: '<a ng-if="ngModel" href="http://twitter.com/share?text=' + "{{'msg.twittersharetext' | translate}}" + '&url=http://smigo.org/wall/{{ngModel}}&via=smigogarden" target="_blank"><span class="social-sprite twitter"></span>&NonBreakingSpace;</a>'
+        template: '<a ng-if="ngModel" href="http://twitter.com/share?text=' + "{{'msg.twittersharetext' | translate}}" + '&url=http://smigo.org/wall/{{ngModel}}&via=smigogarden" target="_blank"><span class="social-sprite twitter"></span>&nbsp;</a>'
     }
 }
 angular.module('smigoModule').directive('twitter', TwitterDirective);
