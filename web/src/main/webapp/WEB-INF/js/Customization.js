a84d4f0e58ec171de6bb7eb679661a4c55ac10b0
Christian <christian1195@gmail.com>
2014-10-28 10:37:55 +0100

    Improved Customization.js

--- web/src/main/webapp/WEB-INF/js/Customization.js
+++ web/src/main/webapp/WEB-INF/js/Customization.js
@@ -1,24 +1,40 @@
-String.prototype.capitalize = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-};
-
-Array.prototype.last = function () {
-    return this.slice(-1)[0];
-}
-Array.prototype.find = function (value, property, options) {
-
-    if (options && options.ignoreCase) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i][property] && this[i][property].toLocaleLowerCase() == value.toLowerCase()) {
-                return this[i];
-            }
-        }
-    } else {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i][property] === value) {
-                return this[i];
-            }
-        }
+Object.defineProperty(String.prototype, 'capitalize', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function () {
+        return this.charAt(0).toUpperCase() + this.slice(1);
     }
-    return null;
-};
+});
+
+Object.defineProperty(Array.prototype, 'last', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function () {
+        if (this.length > 0) return this[this.length - 1];
+    }
+});
+
+Object.defineProperty(Array.prototype, 'find', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function (value, property, options) {
+
+        if (options && options.ignoreCase) {
+            for (var i = 0; i < this.length; i++) {
+                if (this[i][property] && this[i][property].toLocaleLowerCase() == value.toLowerCase()) {
+                    return this[i];
+                }
+            }
+        } else {
+            for (var i = 0; i < this.length; i++) {
+                if (this[i][property] === value) {
+                    return this[i];
+                }
+            }
+        }
+        return null;
+    }
+});
