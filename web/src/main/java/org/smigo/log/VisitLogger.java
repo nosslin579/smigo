0c1aa989ec572d5aa302cc59a9ad9d7d787c2891
Christian <christian1195@gmail.com>
2014-11-01 18:47:52 +0100

    VisitLogger.java simplyfied

--- web/src/main/java/org/smigo/log/VisitLogger.java
+++ web/src/main/java/org/smigo/log/VisitLogger.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Enumeration;
@@ -32,27 +31,23 @@ public class VisitLogger extends HandlerInterceptorAdapter {
         final long start = (Long) request.getAttribute(REQUEST_TIMER);
         log.info("Request finished in " + (System.nanoTime() - start) + "ns which is " + (System.nanoTime() - start) / 1000000 + "ms");
         String requestedURL = request.getRequestURL().toString();
-        if (requestedURL.endsWith(".js") || requestedURL.endsWith(".png") || requestedURL.endsWith(".css") || requestedURL.endsWith(".html")) {
-            log.debug("Not logging " + requestedURL);
-        } else if (request.getDispatcherType() != DispatcherType.FORWARD) {
-            LogBean logBean = LogBean.create(request, response);
+        LogBean logBean = LogBean.create(request, response);
 
-            StringBuilder s = new StringBuilder("Logging request>");
-            s.append(logBean.toString());
-            s.append(" Auth type:");
-            s.append(request.getAuthType());
-            s.append(" Principal:");
-            s.append(request.getUserPrincipal());
-            s.append(" Headers:");
-            Enumeration<String> headerNames = request.getHeaderNames();
-            while (headerNames.hasMoreElements()) {
-                String headerName = headerNames.nextElement();
-                s.append(headerName).append("=").append(request.getHeader(headerName)).append(" - ");
-            }
-
-            log.info(s.toString());
-            logDao.log(logBean);
+        StringBuilder s = new StringBuilder("Logging request>");
+        s.append(logBean.toString());
+        s.append(" Auth type:");
+        s.append(request.getAuthType());
+        s.append(" Principal:");
+        s.append(request.getUserPrincipal());
+        s.append(" Headers:");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            s.append(headerName).append("=").append(request.getHeader(headerName)).append(" - ");
         }
+
+        log.info(s.toString());
+        logDao.log(logBean);
     }
 
 }
