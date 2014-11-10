090e4ac97ab6f120f87920589d3be97183e6e80e
Christian <christian1195@gmail.com>
2014-11-10 14:16:36 +0100

    Update copy script

--- sql/copy.sql
+++ sql/copy.sql
@@ -1,27 +1,33 @@
+DROP TABLE families;
 CREATE TABLE families LIKE nosslin2_db.families;
 INSERT INTO families SELECT * FROM nosslin2_db.families;
 
+DROP TABLE openid;
 CREATE TABLE openid LIKE nosslin2_db.openid;
 INSERT INTO openid SELECT * FROM nosslin2_db.openid;
 
+DROP TABLE persistent_logins;
 CREATE TABLE persistent_logins LIKE nosslin2_db.persistent_logins;
 
+DROP TABLE plants;
 CREATE TABLE plants LIKE nosslin2_db.plants;
 INSERT INTO plants SELECT * FROM nosslin2_db.plants;
 
+DROP TABLE rules;
 CREATE TABLE rules LIKE nosslin2_db.rules;
 INSERT INTO rules SELECT * FROM nosslin2_db.rules;
 
+DROP TABLE species;
 CREATE TABLE species LIKE nosslin2_db.species;
 INSERT INTO species SELECT * FROM nosslin2_db.species;
 
-CREATE TABLE speciestransbylanguage LIKE nosslin2_db.speciestransbylanguage;
-INSERT INTO speciestransbylanguage SELECT * FROM nosslin2_db.speciestransbylanguage;
+DROP TABLE species_translation;
+CREATE TABLE species_translation LIKE nosslin2_db.species_translation;
+INSERT INTO species_translation SELECT * FROM nosslin2_db.species_translation;
 
+DROP TABLE users;
 CREATE TABLE users LIKE nosslin2_db.users;
 INSERT INTO users SELECT * FROM nosslin2_db.users;
 
-CREATE TABLE usersettingforspecies LIKE nosslin2_db.usersettingforspecies;
-INSERT INTO usersettingforspecies SELECT * FROM nosslin2_db.usersettingforspecies;
-
+DROP TABLE visitlog;
 CREATE TABLE visitlog LIKE nosslin2_db.visitlog;
