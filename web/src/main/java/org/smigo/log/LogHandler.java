98128cc0b5369528c87e11e350f32052d3475a69
Christian <christian1195@gmail.com>
2014-12-09 10:02:07 +0100

    Moved log logic to handler

---
+++ web/src/main/java/org/smigo/log/LogHandler.java
@@ -0,0 +1,41 @@
+package org.smigo.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+@Component
+public class LogHandler {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private LogDao logDao;
+
+
+    public void log(HttpServletRequest request, HttpServletResponse response) {
+        String requestedURL = request.getRequestURL().toString();
+        LogBean logBean = LogBean.create(request, response);
+
+        StringBuilder s = new StringBuilder("Logging request>");
+        s.append(logBean.toString());
+        s.append(" Auth type:");
+        s.append(request.getAuthType());
+        s.append(" Principal:");
+        s.append(request.getUserPrincipal());
+        s.append(" Headers:");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            s.append(headerName).append("=").append(request.getHeader(headerName)).append(" - ");
+        }
+
+        log.info(s.toString());
+        logDao.log(logBean);
+
+    }
+}
