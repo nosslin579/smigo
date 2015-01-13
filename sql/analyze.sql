ed63db9eb91e02d5a8e2a89cf8120db992cc999f
Christian <christian1195@gmail.com>
2015-01-13 21:19:01 +0100

    Analyze sql fix

--- sql/analyze.sql
+++ sql/analyze.sql
@@ -11,13 +11,13 @@ SELECT
   count(IF(year = 2013, 1, NULL)) AS y2013,
   count(IF(year = 2014, 1, NULL)) AS y2014,
   count(IF(year = 2015, 1, NULL)) AS y2015
-FROM users
-  LEFT JOIN plants ON plants.user_id = users.id
+FROM nosslin2_db.users
+  LEFT JOIN nosslin2_db.plants ON plants.user_id = users.id
   LEFT JOIN (SELECT
                username,
                count(*)                  AS requests,
                count(DISTINCT sessionid) AS sessions
-             FROM visitlog
+             FROM nosslin2_db.visitlog
              GROUP BY username) AS r ON r.username = users.username
 GROUP BY id
 ORDER BY id DESC;
