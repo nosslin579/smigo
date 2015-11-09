2a38d66d0fb424b98c6b1b068c38c38edafe797e
Christian <christian1195@gmail.com>
2015-11-09 10:51:10 +0100

    Analyze sql update

--- sql/analyze.sql
+++ sql/analyze.sql
@@ -9,9 +9,10 @@ SELECT
   users.createdate,
   requests,
   sessions,
+  count(IF(year = 2016, 1, NULL)) AS y2016,
   count(IF(year = 2015, 1, NULL)) AS y2015,
   count(IF(year = 2014, 1, NULL)) AS y2014,
-  count(IF(year = 2013, 1, NULL)) AS y2013
+  speciescreated
 FROM nosslin2_db.users
   LEFT JOIN nosslin2_db.plants ON plants.user_id = users.id
   LEFT JOIN (SELECT
@@ -20,6 +21,11 @@ FROM nosslin2_db.users
                count(DISTINCT sessionid) AS sessions
              FROM nosslin2_db.visitlog
              GROUP BY username) AS r ON r.username = users.username
+  LEFT JOIN (SELECT
+               creator AS speciescreator,
+               count(creator) AS speciescreated
+             FROM nosslin2_db.species
+             GROUP BY creator) AS sc ON sc.speciescreator = users.id
 GROUP BY id
 ORDER BY id DESC;
 
@@ -30,3 +36,17 @@ FROM nosslin2_db.visitlog
 GROUP BY referer
 ORDER BY count(referer) DESC
 LIMIT 200;
+
+
+SELECT species.id, u.username, def.vernacular_name, numofplants
+FROM nosslin2_db.species
+  JOIN nosslin2_db.species_translation def ON def.species_id = species.id
+  LEFT JOIN nosslin2_db.users u ON u.id = species.creator
+  LEFT JOIN (SELECT
+               species_id,
+               count(species_id) AS numofplants
+             FROM nosslin2_db.plants
+             GROUP BY species_id) AS pc ON pc.species_id = species.id
+GROUP BY species.id
+ORDER BY species.id DESC
+LIMIT 500;
