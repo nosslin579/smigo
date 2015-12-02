43e162e03ff597cf7ed7c4d8eb7658d96012a459
Christian <christian1195@gmail.com>
2015-12-02 20:33:07 +0100

    Made variety selectable

--- web/src/main/webapp/WEB-INF/js/species/SpeciesModalController.js
+++ web/src/main/webapp/WEB-INF/js/species/SpeciesModalController.js
@@ -8,16 +8,18 @@ function SpeciesModalController($log, $scope, $uibModalInstance, SpeciesService,
         SpeciesService.selectSpecies(SpeciesService.getSpecies(speciesId));
         $uibModalInstance.dismiss('selected species');
     };
-
     $scope.close = function () {
         $uibModalInstance.dismiss('close');
     };
-
     $scope.addVariety = function (form, speciesId) {
         $log.log('Add Variety:', form);
         SpeciesService.addVariety(form.varietyName, speciesId, StateService.getUser().currentUser.id);
         form.varietyName = '';
         form.visible = false;
+    };
+    $scope.toggleVariety = function (variety, species, event) {
+        $log.log('Toggle variety:', [variety, species, event]);
+        species.variety = species.variety == variety ? null : variety;
     };
 }
 
