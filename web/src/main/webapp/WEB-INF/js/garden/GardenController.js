5c15ddecec3e0e06d282d2560bf4c0606f0e64a1
Christian <christian1195@gmail.com>
2015-12-05 11:03:21 +0100

    Scroll to selected species added

--- web/src/main/webapp/WEB-INF/js/garden/GardenController.js
+++ web/src/main/webapp/WEB-INF/js/garden/GardenController.js
@@ -1,4 +1,4 @@
-function GardenController($http, $log, $uibModal, $scope, $filter, $timeout, StateService, SpeciesService, UserService) {
+function GardenController($http, $log, $uibModal, $scope, $filter, $location, $anchorScroll, $timeout, StateService, SpeciesService, UserService) {
 
     $scope.search = {query: '', proccessing: false};
     $scope.clickAgainToOpenTooltipEnable = true;
@@ -12,13 +12,27 @@ function GardenController($http, $log, $uibModal, $scope, $filter, $timeout, Sta
     $scope.searchSpecies = SpeciesService.searchSpecies;
     $scope.isSpeciesAddable = SpeciesService.isSpeciesAddable;
 
-    $scope.selectedSpeciesFromTopResult = function (query) {
-        $log.log('Setting species from', query);
+    $scope.selectedSpeciesFromTopResult = function (query, event) {
+        $log.log('Setting species from', [query, event]);
         var topResult = $filter('speciesFilter')(SpeciesService.getAllSpecies(), query)[0];
         if (topResult) {
             SpeciesService.selectSpecies(topResult);
         }
         $scope.search.query = '';
+
+        //make sure selected species is visible in species scroll list
+        $timeout(function () {
+            var speciesArrayAlphabetically = $filter('orderBy')(SpeciesService.getAllSpecies(), 'vernacularName');
+            var indexOfSelectedSpecies = speciesArrayAlphabetically.indexOf(topResult);
+            var speciesAboveSelected = speciesArrayAlphabetically[(indexOfSelectedSpecies - 4)];
+            $log.log('Scrolling to(or not if undefined):', speciesAboveSelected);
+            speciesAboveSelected && $anchorScroll('select-species-' + speciesAboveSelected.id);
+            event.currentTarget[0].blur();
+        });
+    };
+    $scope.getTopResult = function (query) {
+        var topResult = $filter('speciesFilter')(SpeciesService.getAllSpecies(), query)[0];
+        return topResult ? topResult : '-';
     };
     $scope.openAddYearModal = function () {
         $uibModal.open({
