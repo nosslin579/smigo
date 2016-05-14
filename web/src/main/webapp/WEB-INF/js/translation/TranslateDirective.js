5330f219bf6c2a80d0540e616f71fc0c42ab0c0f
Christian <christian1195@gmail.com>
2016-05-14 15:37:14 +0200

    Migration to translate directive

--- web/src/main/webapp/WEB-INF/js/translation/TranslateDirective.js
+++ web/src/main/webapp/WEB-INF/js/translation/TranslateDirective.js
@@ -1,7 +1,7 @@
 angular.module('smigoModule').directive('soMsg', function TranslateDirective($log, TranslateService) {
     return {
         link: function (scope, element, attrs) {
-            $log.log('TranslateDirective link', [scope, element, attrs]);
+            //$log.log('TranslateDirective link', [scope, element, attrs]);
             element.text(TranslateService.translate(attrs.soMsg));
             scope.$on('get-translation-success', function (event, allMessages) {
                 //$log.info('TranslateDirective get-translation-success', [scope, element, attrs, allMessages]);
