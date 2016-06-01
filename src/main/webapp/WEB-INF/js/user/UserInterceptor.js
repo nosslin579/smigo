04fe11070771ddac1021ac603cf0938879ff295e
Christian <christian1195@gmail.com>
2016-06-01 11:28:00 +0200

    UserInterceptor.js fix

--- src/main/webapp/WEB-INF/js/user/UserInterceptor.js
+++ src/main/webapp/WEB-INF/js/user/UserInterceptor.js
@@ -1,15 +1,18 @@
-angular.module('smigoModule').factory('UserInterceptor', function UserInterceptor($log, $rootScope) {
-
-    function broadCastIfNotGet(response) {
-        //$log.info('responset', [response.headers('SmigoUser'), response.headers(), response]);
-        if (response.config.method !== 'GET') {
-            $rootScope.$broadcast('response-received', response);
-        }
-        return response;
-    }
+angular.module('smigoModule').factory('UserInterceptor', function UserInterceptor($log, $rootScope, $q) {
 
     return {
-        responseError: broadCastIfNotGet,
-        response: broadCastIfNotGet
+        responseError: function broadCastIfNotGet(response) {
+            if (response.config.method !== 'GET') {
+                $rootScope.$broadcast('response-received', response);
+            }
+            return $q.reject(response);
+        },
+        response: function broadCastIfNotGet(response) {
+            //$log.info('responset', [response.headers('SmigoUser'), response.headers(), response]);
+            if (response.config.method !== 'GET') {
+                $rootScope.$broadcast('response-received', response);
+            }
+            return response;
+        }
     }
 });
